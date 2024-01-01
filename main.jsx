150e2e45cdcfa024f3ba89d3f415b0d5461bee91
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2024-01-01 19:17:31 +0530

    Add Router Provider for Routing

--- main.jsx
+++ main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./src/App.jsx";
+import appRouter from "./src/App";
+import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>
 );
