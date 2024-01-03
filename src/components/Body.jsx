d32ac157b05cf60cf824cc5a9eb0406c38595954
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2024-01-03 12:19:56 +0530

    Take all the state variables as props and deployed the required functionality

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,27 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import EmployeeCard from "./EmployeeCard";
+import { useOutletContext } from "react-router-dom";
 
 const EMPLOYEE_API = "https://jsonplaceholder.typicode.com/users";
 
 const Body = () => {
-  const [employeeData, setEmployeeData] = useState([]);
+  // const [employeeData, setEmployeeData] = useState([]);
+  const [
+    employeeData,
+    setEmployeeData,
+    showData,
+    setShowData,
+    searchQuery,
+    setSearchQuery,
+  ] = useOutletContext();
 
   const employeeDetails = async () => {
     const data = await fetch(EMPLOYEE_API);
     const json = await data.json();
-    console.log(json);
+    // console.log(json);
     setEmployeeData(json);
+    setShowData(json);
   };
 
   useEffect(() => {
@@ -21,15 +31,18 @@ const Body = () => {
   return (
     <>
       <div className="card-container">
-        {employeeData.map((employeeData) => {
-          return (
+        {showData &&
+          showData.map((employeeData) => (
             <li key={employeeData.id}>
-              <EmployeeCard data={employeeData} />
+              <EmployeeCard
+                data={employeeData}
+                {...{ setShowData, showData }}
+              />
             </li>
-          );
-        })}
+          ))}
       </div>
     </>
   );
 };
+
 export default Body;
