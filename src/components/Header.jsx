ef387a413d3f4d25963e76eb70b13b1d786e0be4
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2024-01-03 12:15:04 +0530

    Add Search Functionality in the Search Bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,29 @@
-const Header = () => {
+import { useState } from "react";
+
+const Header = ({
+  employeeData,
+  setEmployeeData,
+  showData,
+  setShowData,
+  searchQuery,
+  setSearchQuery,
+}) => {
+  // const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (query) => {
+    // const query = e.target.value;
+    // console.log(query);
+    setSearchQuery(query);
+
+    const filterData = employeeData.filter((item) =>
+      item.id.toString().includes(query)
+    );
+
+    // console.log(filterData);
+
+    setShowData(filterData);
+  };
+
   return (
     <>
       {/* Header Container */}
@@ -11,7 +36,11 @@ const Header = () => {
 
         {/* Search Bar */}
         <div className="search-bar">
-          <input placeholder="Search for an Employee" />
+          <input
+            placeholder="Search for an Employee"
+            value={searchQuery}
+            onChange={(e) => handleSearch(e.target.value)}
+          />
           <i class="fa-solid fa-magnifying-glass search-icon"></i>
         </div>
 
