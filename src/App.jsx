8ea5a7af762120f8c6b80c526d73e412000e8635
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2024-01-03 12:14:34 +0530

    Add all the state variable to pass on as props in components and as context in Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,36 @@ import { Outlet, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { useState } from "react";
 
 function App() {
+  const [employeeData, setEmployeeData] = useState([]);
+  const [showData, setShowData] = useState(employeeData);
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <>
       <div>
-        <Header />
-        <Outlet />
+        <Header
+          {...{
+            employeeData,
+            setEmployeeData,
+            showData,
+            setShowData,
+            searchQuery,
+            setSearchQuery,
+          }}
+        />
+        <Outlet
+          context={[
+            employeeData,
+            setEmployeeData,
+            showData,
+            setShowData,
+            searchQuery,
+            setSearchQuery,
+          ]}
+        />
       </div>
     </>
   );
